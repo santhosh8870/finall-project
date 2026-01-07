60a8cd86d482e35ec2237dcd977aa670c26d3f27
Your Name <you@example.com>
2026-01-07 12:51:15 +0530

    update app

--- frontend/src/components/layouts/Loader.js
+++ frontend/src/components/layouts/Loader.js
@@ -1,5 +1,12 @@
-export default function Loader() {
-    return (
-        <div className="loader"></div>
+export default function Loader(){
+    return(
+        
+        <div className="main-load">
+            <div className="loader">
+                <div className="circle"></div>
+            </div>
+        </div>
+        
+
     )
 }
